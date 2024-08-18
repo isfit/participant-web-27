2a5288e16fbed8daf7500789577a2350ccc331df
Marcus Stephan Nordal <marcusn1301@gmail.com>
2024-08-18 23:15:40 +0200

    Application form bug fix

--- frontend/src/pages/applicationForm/ApplicationForm.tsx
+++ frontend/src/pages/applicationForm/ApplicationForm.tsx
@@ -72,16 +72,16 @@ const ApplicationForm: React.FC = () => {
   }, [currentStep, visitedSteps]);
 
   const handleChange = (
-    e: React.ChangeEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
   ) => {
-    const { name, value } = e.target;
-    const isCheckbox =
-      e.target instanceof HTMLInputElement && e.target.type === 'checkbox';
+    const { name, value, type } = e.target;
+    const isCheckbox = type === 'checkbox';
+  
     setFormValues((prevState) => ({
       ...prevState,
-      [name]: isCheckbox ? e.target.checked : value,
+      [name]: isCheckbox
+        ? (e.target as HTMLInputElement).checked 
+        : value,
     }));
   };
 
@@ -108,6 +108,7 @@ const ApplicationForm: React.FC = () => {
   };
 
   const handleNext = () => {
+    console.log(currentStep)
     if (validateStep()) {
       if (currentStep < steps.length - 1) {
         setCurrentStep(currentStep + 1);
@@ -127,9 +128,12 @@ const ApplicationForm: React.FC = () => {
     }
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     
+    if (!validateStep()) {
+      return;
+    }
+
     const formData = new FormData();
     
     Object.entries(formValues).forEach(([key, value]) => {
@@ -145,6 +149,8 @@ const ApplicationForm: React.FC = () => {
       const response = await apply(formData);
       if (response.status === 201) {
         setSubmitted(true);
+        console.log('Application submitted:', response.data.message);
+        localStorage.removeItem('applicationForm');
         setTimeout(() => {
           setRedirect(true);
         }, 5000);
@@ -325,54 +331,6 @@ const ApplicationForm: React.FC = () => {
     }
   };
 
-  const personalDetails = [
-    { label: 'Full Name', name: 'fullName', type: 'text', placeholder: 'John Doe', required: true },
-    { label: 'Date of Birth', name: 'dateOfBirth', type: 'date', placeholder: 'YYYY-MM-DD', required: true },
-    { label: 'Gender', name: 'gender', type: 'select', options: ['Male', 'Female', 'Other'], required: true },
-    { label: 'Nationality', name: 'nationality', type: 'text', placeholder: 'ex. Norwegian', required: true },
-    { label: 'Continent of Nationality', name: 'continent', type: 'text', placeholder: 'ex. Europe', required: true },
-    { label: 'Country of Residence', name: 'residenceCountry', type: 'text', placeholder: 'ex. Norway', required: true },
-    { label: 'By checking this box, I confirm I am a student throughout the academic year 2024-2025', name: 'isStudent', type: 'checkbox', required: true },
-    { label: 'What do you study?', name: 'studyField', type: 'text', placeholder: 'ex. Infomatics', required: true },
-    { label: 'Name of your University/Institute', name: 'university', type: 'text', placeholder: 'ex. Norwegian University of Science and Technology', required: true },
-    { label: 'Your University/Institute website address (optional)', name: 'universityWebsite', type: 'text', placeholder: 'ex. https://youruniversity.edu' },
-    { label: 'Please upload your student certificate', name: 'studentCertificate', type: 'file', required: true },
-    { label: 'By checking this box, I confirm I am able to communicate in English', name: 'isEnglishSpeaker', type: 'checkbox', required: true },
-    { label: 'What is your T-Shirt size?', name: 'tShirtSize', type: 'select', options: ['XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL'], required: true },
-    { label: 'I am applying as', name: 'applyingAs', type: 'select', options: ['Regular participant', 'SOrCE', 'Coastal Carolina University', 'Dialogue Project'], required: true },
-  ];
-
-  const themeSection = [
-    { label: "When considering the theme of 'POWER' for ISFiT25, what aspects or dimensions of power come to your mind first?", name: 'themePowerThoughts', type: 'textarea', placeholder: 'When I think of power I think of ...', required: true },
-    { label: "Reflecting on your country's context, can you identify a specific power issue? How does this issue manifest, and what are its consequences?", name: 'countryPowerIssue', type: 'textarea', placeholder: 'When reflecting on my country\'s context, an issue regarding power is ...', required: true },
-    { label: 'What is your motivation for attending ISFiT25? How do you envision contributing to discussions and activities surrounding this theme during the festival?', name: 'motivation', type: 'textarea', placeholder: 'My motivation for attending ISFiT25 is ...', required: true },
-  ];
-
-  const workshopSection = [
-    { label: 'Select your first priority', name: 'firstPriority', type: 'text', placeholder: 'Your first priority', required: true },
-    { label: 'Why do you want to attend this workshop? Please give examples of how this workshop relates to your life or to the theme Polarization.', name: 'firstPriorityReason', type: 'textarea', placeholder: 'I want to attend this workshop because ...', required: true },
-    { label: 'Select your second priority (optional)', name: 'secondPriority', type: 'text', placeholder: 'Your second priority', required: true },
-    { label: 'Why do you want to attend this workshop? Please give examples of how this workshop relates to your life or to the theme Power. (optional)', name: 'secondPriorityReason', type: 'textarea', placeholder: 'I want to attend this workshop because ...' },
-    { label: 'Select your third priority (optional)', name: 'thirdPriority', type: 'text', placeholder: 'Your third priority', required: true },
-    { label: 'Why do you want to attend this workshop? Please give examples of how this workshop relates to your life or to the theme Power. (optional)', name: 'thirdPriorityReason', type: 'textarea', placeholder: 'I want to attend this workshop because ...' },
-    { label: 'By checking this box, I confirm I am aware it is mandatory to participate in the given workshop even if I don´t get any of my priorities', name: 'workshopMandatory', type: 'checkbox', required: true },
-  ];
-
-  const financialSupportSection = [
-    { label: 'Some of the participants get financial support for their trip to participate in ISFiT25. Why do you think that you should be considered for this financial support?', name: 'financialSupportReason', type: 'textarea', placeholder: 'I should be considered to get financial support because ...' },
-    { label: 'How many dependents do you have?', name: 'dependents', type: 'number', placeholder: 'Number of dependents' },
-    { label: 'What is your family´s monthly income?', name: 'familyIncome', type: 'text', placeholder: 'Your family income' },
-    { label: 'I can participate in ISFiT25', name: 'canParticipate', type: 'select', options: ['without any financial support', 'if I get partial financial support', 'if I get full financial support'] },
-  ];
-
-  const consentSection = [
-    { label: 'I have to apply for a visa if I get accepted as a participant', name: 'consentVisa', type: 'checkbox', required: true },
-    { label: 'I am aware that I have to book a flight to Norway on my own even if I get financial support', name: 'consentFlight', type: 'checkbox', required: true },
-    { label: 'I agree that ISFiT can keep my personal details to be used later in the festival', name: 'consentPersonalDetails', type: 'checkbox', required: true },
-    { label: 'I am aware that a participation certificate will be given only if I attend all days of the workshop and mandatory events', name: 'consentAttendance', type: 'checkbox', required: true },
-    { label: 'I agree that ISFiT can share pictures and videos of me taken during the festival on social media', name: 'consentMedia', type: 'checkbox', required: true },
-  ];
-
   const renderStep = () => {
     return getCurrentFields().map(renderInput);
   };
@@ -388,60 +346,67 @@ const ApplicationForm: React.FC = () => {
         </Link>
       </div>
       <Header linkTo="/homepage" />
-
+  
       {submitted ? (
         <div className="outerContainer">
           <h1 className="applicationSectionHeader">Application submitted</h1>
           <p>Thank you for applying to ISFiT!</p>
         </div>
       ) : (
-      <>
-      <div className="outerContainer">
-        <h1 className="applicationSectionHeader">Application Form</h1>
-        <p>
-          Hey! Awesome that you want to apply for ISFiT 2025, please fill out
-          all required fields.
-        </p>
-      </div>
-
-      <div className="progressOverview">
-        {steps.map((step, index) => (
-          <span key={index}>
-            <span
-              className={`progressStep ${index === currentStep ? 'currentStep' : ''} ${
-                visitedSteps.includes(index) ? 'clickableStep' : ''
-              }`}
-              onClick={() => handleStepClick(index)}
-            >
-              {step}
-            </span>
-            {index < steps.length - 1 && <span className="separator"> &gt; </span>}
-          </span>
-        ))}
-      </div>
-
-      <form className="applicationForm" onSubmit={handleSubmit}>
-        <div id="applicationContainer">
+        <>
           <div className="outerContainer">
-            <h1 className="applicationSectionHeader">{steps[currentStep]}</h1>
-            {renderStep()}
+            <h1 className="applicationSectionHeader">Application Form</h1>
+            <p>
+              Hey! Awesome that you want to apply for ISFiT 2025, please fill out
+              all required fields.
+            </p>
           </div>
-        </div>
-        <div className="navigationButtons">
-          {currentStep > 0 && (
-            <Button onClick={handlePrevious}>Previous</Button>
-          )}
-          {currentStep < steps.length - 1 ? (
-            <Button onClick={handleNext}>Next</Button>
-          ) : (
-            <Button type="submit">Submit</Button>
-          )}
-        </div>
-      </form>
-      </>
-      )};
+  
+          <div className="progressOverview">
+            {steps.map((step, index) => (
+              <span key={index}>
+                <span
+                  className={`progressStep ${
+                    index === currentStep ? 'currentStep' : ''
+                  } ${visitedSteps.includes(index) ? 'clickableStep' : ''}`}
+                  onClick={() => handleStepClick(index)}
+                >
+                  {step}
+                </span>
+                {index < steps.length - 1 && (
+                  <span className="separator"> &gt; </span>
+                )}
+              </span>
+            ))}
+          </div>
+
+          <form id="applicationContainer">
+            <div className="outerContainer">
+              <h1 className="applicationSectionHeader">{steps[currentStep]}</h1>
+              {renderStep()}
+            </div>
+          </form>
+  
+          <div className="navigationButtons">
+            {currentStep > 0 && (
+              <Button type="button" onClick={handlePrevious}>
+                Previous
+              </Button>
+            )}
+            {currentStep < steps.length - 1 ? (
+              <Button type="button" onClick={handleNext}>
+                Next
+              </Button>
+            ) : (
+              <Button type="button" onClick={handleSubmit}>
+                Apply
+              </Button>
+            )}
+          </div>
+        </>
+      )}
     </div>
-  );
-};
+  )
+}
 
 export default ApplicationForm;
