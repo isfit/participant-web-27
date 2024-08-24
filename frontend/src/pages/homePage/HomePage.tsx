4777013699c5345b9110aea766d6415656f11454
Steffen Hæren <steffenhaeren@gmail.com>
2024-08-24 14:14:05 +0200

    made login and createuser responsive, added hamburger menu on homepage

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -1,58 +1,66 @@
 import React from 'react';
 import { Heading, Text, Box, Button } from '@radix-ui/themes';
 import { Link } from 'react-router-dom';
-import './HomePage.css';
+import styles from './HomePage.module.css';
 import { useAuth } from '../../context/AuthenticationContext';
 import { ROLES } from '../../config/roles';
 
-
-const AboutPage: React.FC = () => {
+const HomePage: React.FC = () => {
   const { user } = useAuth();
   return (
-    <div className="loginNavbar">
-      <div className="topRight">
-        {user?.role === ROLES.ADMIN && <Link to="/admin" style={{ color: 'white'}}><Button>Admin</Button></Link>}
-        <Link to="/login" style={{ color: 'white'}}><Button>Login</Button></Link>
-        <Link to="/profilePage" style={{ color: 'white'}}><Button>My profile</Button></Link>
+    <div>
+      <div className={styles.hamburgerMenu}>
+        <div className={styles.hamburgerIcon}></div>
+        <div className={styles.hamburgerIcon}></div>
+        <div className={styles.hamburgerIcon}></div>
+        <div className={styles.menuContent}>
+          {user?.role === ROLES.ADMIN && (
+            <Link to="/admin">Admin</Link>
+          )}
+          <Link to="/login">Login</Link>
+          <Link to="/profilePage">My profile</Link>
+        </div>
       </div>
-     
-      <div className="center-container">
-      <img src={'/isfit_log.png'} alt="Logo" className="logo" />
-    </div>
+
+      <div className={styles.centerContainer}>
+        <img src={'/isfit_log.png'} alt="Logo" className={styles.logo} />
+      </div>
       <br />
-      <Link to="/applicationForm" className="applyButtonLink"><Button className="applyButton">Apply now!</Button></Link>
+      <Link to="/applicationForm" className={styles.applyButtonLink}>
+        <Button className={styles.applyButton}>Apply now!</Button>
+      </Link>
       <Box>
-        <Heading as="h1" size={"1"}>
+        <Heading as="h1" size="1">
           About ISFIT
         </Heading>
-        <Text as="p" size={"4"}>
-          ISFiT is the world’s largest international student festival,taking place biennially during the 
-          spring semester in Trondheim, Norway. We gather students from different national and cultural 
-          backgrounds to talk and connect through exciting and engaging topics and discussions. The first 
+        <Text as="p" size="4">
+          ISFiT is the world’s largest international student festival, taking place biennially during the
+          spring semester in Trondheim, Norway. We gather students from different national and cultural
+          backgrounds to talk and connect through exciting and engaging topics and discussions. The first
           ever ISFiT festival was held in 1990 and ever since people from around the world has gathered
-          in Trondheim every other year in unison. The next festival will happen in 2025. Each festival 
+          in Trondheim every other year in unison. The next festival will happen in 2025. Each festival
           explores a different theme, and this festival's theme is POWER!
         </Text>
         <Text as="p">
-          In a world driven by various forms of power, ISFIT25 encourages a reflective journey into 
-          understanding and redefining the concept. Power, often associated with authority and control, 
-          extends beyond conventional boundaries. At ISFIT, we believe in harnessing the strength of 
-          influence to bring about positive change. 
+          In a world driven by various forms of power, ISFIT25 encourages a reflective journey into
+          understanding and redefining the concept. Power, often associated with authority and control,
+          extends beyond conventional boundaries. At ISFIT, we believe in harnessing the strength of
+          influence to bring about positive change.
         </Text>
-        <Heading as="h2" size={"1"}>
+        <Heading as="h2" size="1">
           The Student Peace Prize
         </Heading>
-        <Text as="p" size={"4"}>
-          The Student Peace Prize (SPP) was established in 1999 – as an initiative by volunteers from the 
-          International Student Festival in Trondheim (ISFiT). It is awarded biennially to a student or 
-          student organization working to promote peace, human rights and democracy. The Prize is awarded 
-          on behalf of all students in Norway, while an independent Student Peace Prize Committee makes 
-          the selection itself. The election happens through the independent committee for the Students 
+        <Text as="p" size="4">
+          The Student Peace Prize (SPP) was established in 1999 – as an initiative by volunteers from the
+          International Student Festival in Trondheim (ISFiT). It is awarded biennially to a student or
+          student organization working to promote peace, human rights and democracy. The Prize is awarded
+          on behalf of all students in Norway, while an independent Student Peace Prize Committee makes
+          the selection itself. The election happens through the independent committee for the Students
           Peace Prize, while the award itself, takes place during ISFiT’s own award ceremony.
         </Text>
       </Box>
     </div>
-    );
+  );
 };
 
-export default AboutPage;
+export default HomePage;
