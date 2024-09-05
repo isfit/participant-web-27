0d0b1e4571c8e61f19b743ad44f54d0f1b95b439
Steffen Hæren <steffenhaeren@gmail.com>
2024-09-05 22:05:25 +0200

    chat yap

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
+import express, { Express, Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import user from './routes/user';
@@ -13,39 +13,39 @@ const app: Express = express();
 
 // CORS configuration
 const allowedOrigins = [
-  'https://participant-web-25-dwgcdgbbd3d4cmd6.norwayeast-01.azurewebsites.net/', // Deployed frontend URL
-  'http://localhost:5173'  // Local frontend for development
+  'https://participant-web-25-dwgcdgbbd3d4cmd6.norwayeast-01.azurewebsites.net',  // Your frontend URL
+  'http://localhost:5173',  // Local development URL
 ];
 
 const corsOptions = {
   origin: function (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);  // Allow request if the origin is in the allowedOrigins list
+      callback(null, true);  // Allow the request if the origin is in the allowedOrigins list
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true  // Allow cookies or other credentials to be sent
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Specify allowed methods
+  credentials: true,  // Allow credentials (cookies, etc.)
 };
 
-// Apply CORS middleware
+
+// Use CORS middleware
 app.use(cors(corsOptions));
 
-// Parse incoming requests
+// Middleware for parsing requests
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // MongoDB connection
-const uri: string =
-  process.env.MONGODB_URI || 'mongodb://participant-web-25-server:9KnBdV0x9XR8caPKd4qIk8vsNT7q8PkdsHySm5x62QmKCA72nV8evp6WBL0UIum3LU0eQRIZyYigACDbyAqbvA==@participant-web-25-server.mongo.cosmos.azure.com:10255/participant-web-25-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@participant-web-25-server@';
-
+const uri: string = process.env.MONGODB_URI || 'mongodb://participant-web-25-server:9KnBdV0x9XR8caPKd4qIk8vsNT7q8PkdsHySm5x62QmKCA72nV8evp6WBL0UIum3LU0eQRIZyYigACDbyAqbvA==@participant-web-25-server.mongo.cosmos.azure.com:10255/participant-web-25-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@participant-web-25-server@';
 (async () => {
   try {
     await mongoose.connect(uri);
     console.log('Connected to the database');
-  } catch {
-    console.log('Error connecting to the database');
+  } catch (error) {
+    console.log('Error connecting to the database', error);
   }
 })();
 
