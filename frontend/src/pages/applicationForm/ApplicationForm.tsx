94f1e9f7be0d847ded8c497f33ee091534e636ce
Steffen Hæren <steffenhaeren@gmail.com>
2024-09-05 18:37:34 +0200

    y

--- frontend/src/pages/applicationForm/ApplicationForm.tsx
+++ frontend/src/pages/applicationForm/ApplicationForm.tsx
@@ -435,7 +435,7 @@ const ApplicationForm: React.FC = () => {
             </label>
             {formValues[name] && formValues[name] instanceof File && (
               <div className="fileInfo">
-                <p>Uploaded file: {(formValues[name] as File).name}</p>
+                <p>Uploaded file: {(formValues[name] as unknown as File).name}</p>
               </div>
             )}
           </div>
