6363ae63b2749eded84fbb63387d318d17434a54
Steffen Hæren <steffenhaeren@gmail.com>
2024-09-17 19:29:33 +0200

    added back variables

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { ROLES } from '../config/roles';
+import axiosInstance from './axios';
 
 const api_url: string = import.meta.env.VITE_API_URL;
 
@@ -13,8 +15,18 @@ export const register = async (
     password: password,
     role: ROLES.USER,
   });
+  const config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `${api_url}/auth/register`,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: data,
+  };
 
   try {
+    const response = await axiosInstance.request(config);
     return { success: true };
   } catch (error: any) {
     return { success: false, message: error.response.data.errors[0].msg };
